8b0dbfd41dab0e6eff4e18c17c861cec564a1a33
Pietra Ferreira <pietra.c.silva@aluno.senai.br>
2025-04-14 12:21:53 -0300

    atualizado aula 14

---
+++ exercicios/ex08_02.js
@@ -0,0 +1,10 @@
+//Crie uma lista com os nomes de três amigos e imprima cada nome.
+const prompt = require('prompt-sync')();
+
+let listaNome = ['Pietra', 'Maria Luiza', 'Lara Hara']
+//para cada nome da lista nomes, escreva um nome
+for(let nome of listaNome){
+    console.log(nome);
+   
+}
+
